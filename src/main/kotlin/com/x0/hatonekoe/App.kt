393e76d66b53a733be71c484eb1e1bdea690b64f
ハトネコエ <hato.nekoe@gmail.com>
2017-02-27 06:08:10 +0900

    Set minimum window size

--- src/main/kotlin/com/x0/hatonekoe/App.kt
+++ src/main/kotlin/com/x0/hatonekoe/App.kt
@@ -2,12 +2,20 @@ package com.x0.hatonekoe
 
 import com.x0.hatonekoe.view.MainView
 import javafx.application.Application
+import javafx.stage.Stage
 import tornadofx.App
 
 fun main(args: Array<String>) {
     Application.launch(TornadoMemoApp::class.java, *args)
 }
 
-class TornadoMemoApp : App() {
+class TornadoMemoApp: App() {
     override val primaryView = MainView::class
+
+    override fun start(stage: Stage) {
+        super.start(stage)
+        stage.isFullScreen = false
+        stage.minWidth  = 280.0
+        stage.minHeight = 150.0
+    }
 }

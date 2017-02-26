2e13475106eaca94d791adc4b58862ab587ad61b
ハトネコエ <hato.nekoe@gmail.com>
2017-02-26 22:19:49 +0900

    fxml view

--- src/main/kotlin/com/x0/hatonekoe/views/MainView.kt
+++ src/main/kotlin/com/x0/hatonekoe/views/MainView.kt
@@ -1,20 +1,23 @@
 package com.x0.hatonekoe.views
 
-import javafx.scene.control.Button
-import tornadofx.*
+import javafx.scene.control.Label
+import javafx.scene.layout.BorderPane
+import tornadofx.View
+import tornadofx.button
+import tornadofx.plusAssign
 
 class MainView: View("TornadoMemo") {
+    override val root: BorderPane by fxml("/styles/MainView.fxml")
     val menuBarView: MenuBarView by inject()
-
-    override val root = borderpane {
-        top = menuBarView.root
-        center = vbox()
-        bottom = label("Hello, pretty user!")
-    }
+    val charCounterLabel: Label by fxid("charCounter")
+    val count = 5
 
     init {
-        with(root.center) {
-            this += Button("Push me!")
+        charCounterLabel.text = count.toString()
+
+        with(root) {
+            this.top = menuBarView.root
+            this.center += button("Push me!")
         }
     }
 }

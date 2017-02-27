a03815e9412cd1c0fbb7f61012704aabdd805076
ハトネコエ <hato.nekoe@gmail.com>
2017-02-28 06:40:11 +0900

    Remove unnecessary variable

--- src/main/kotlin/com/x0/hatonekoe/controller/MainController.kt
+++ src/main/kotlin/com/x0/hatonekoe/controller/MainController.kt
@@ -1,11 +1,8 @@
 package com.x0.hatonekoe.controller
 
-import com.x0.hatonekoe.model.TextDocument
-import com.x0.hatonekoe.model.TextDocumentModel
 import com.x0.hatonekoe.view.MainView
 import tornadofx.Controller
 
 class MainController: Controller() {
     val mainView: MainView by inject()
-    val textDocModel = TextDocumentModel(TextDocument())
 }

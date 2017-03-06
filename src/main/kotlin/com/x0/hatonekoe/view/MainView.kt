30921bc3358aa8ee1214b1db3b7316588a99db9d
ハトネコエ(nekonenene) <hato.nekoe@gmail.com>
2017-03-06 20:42:58 +0900

    Refactoring: Use shortcuts in MainView (Thanks, edvin!!)

--- src/main/kotlin/com/x0/hatonekoe/view/MainView.kt
+++ src/main/kotlin/com/x0/hatonekoe/view/MainView.kt
@@ -9,16 +9,13 @@ import tornadofx.borderpane
 
 class MainView: View("TornadoMemo") {
     private val mainController: MainController by inject()
-    val menuBarView: MenuBarView by inject()
-    val textAreaView: TextAreaView by inject()
-    val footerView: FooterView by inject()
 
     override val root = borderpane {
         println("init Main View")
 
-        top = menuBarView.root
-        center = textAreaView.root
-        bottom = footerView.root
+        top(MenuBarView::class)
+        center(TextAreaView::class)
+        bottom(FooterView::class)
     }
 
     init {
